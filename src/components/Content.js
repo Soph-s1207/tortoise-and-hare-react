aecd95ad7a704a6f99ace223c22bac9ed8985c4b
Soph-s1207 <so335888@ucf.edu>
2025-02-01 11:30:56 -0500

    story add

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Content() {
+  return (
+    <main>
+      <p>
+        Once upon a time, a speedy hare constantly mocked a slow-moving tortoise.
+        Tired of the teasing, the tortoise challenged the hare to a race. Overconfident
+        in his abilities, the hare quickly took the lead but decided to rest mid-race.
+        Meanwhile, the tortoise continued at a steady pace and, to everyone's surprise,
+        won the race while the hare slept.
+      </p>
+    </main>
+  );
+}
+
+export default Content;
